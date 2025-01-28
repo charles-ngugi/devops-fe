4ed5984522d5ea2a25533a28e568592dc72c5c8b
charles-ngugi <sammwangi069@gmail.com>
2025-01-28 12:32:10 +0300

    feat: add About page

---
+++ src/About.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import './App.css';
+
+function About() {
+  return <h2>This is an About page</h2>;
+}
+
+export default About;
+
--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'; 
+import About from './About'; 
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, React! Welcome to the DevOps FE App.</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Hello, React! Welcome to the DevOps FE App.</h1>
+
+        {/* Navigation links */}
+        <nav>
+          <Link to="/">Home</Link> | <Link to="/about">About</Link>
+        </nav>
+
+        {/* Routing */}
+        <Routes>
+          {/* The home page (default route) */}
+          <Route path="/" element={<h2>Home Page</h2>} />
+
+          {/* The about page */}
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
 export default App;
+
