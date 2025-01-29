1f0bd1fad01fa37477ffe5b5ceb5f334ae1509cb
charles-ngugi <sammwangi069@gmail.com>
2025-01-29 16:02:07 +0300

    fix: fix Security group ID issue

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -2,10 +2,10 @@ module "vpc" {
   source = "../../modules/vpc"
 
   vpc_cidr            = "10.0.0.0/16"
-  public_subnet_cidr  = "10.0.1.0/24" 
-  private_subnet_cidr = "10.0.2.0/24" 
+  public_subnet_cidr  = "10.0.1.0/24"
+  private_subnet_cidr = "10.0.2.0/24"
   availability_zone   = "af-south-1a"
-  igw_name            = "dev-igw"  
+  igw_name            = "dev-igw"
 }
 
 module "security" {
